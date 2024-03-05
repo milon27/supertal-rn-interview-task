077d2d33282b086131022d44a32257e528aaa64c
milon27 <mdjahidulislammilon@gmail.com>
2024-03-06 00:50:51 +0600

    added .env

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,5 @@
 export const EnvConfig = {
-    API_URL: "https://api.themoviedb.org/3",
-    API_READ_AT:
-        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYzJmOGExZGNiNjY3MTBkYzc5M2EwMDIwZjVlOGZiNyIsInN1YiI6IjY1ZTZkNDExNWY2YzQ5MDE4NjQ3MzkwMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ewLfcXywYvJI64J2CRqV6wWsrGhOybQw6WgTd-sMrAs",
+    API_URL: process.env.API_URL,
+    API_READ_AT: process.env.API_READ_AT,
     IMAGE_PREFIX: "https://media.themoviedb.org/t/p/w440_and_h660_face",
 }
